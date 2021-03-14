bb64e1be4bc9ff7a6f3d7cc72ff70993c4facbeb
Mike Hurt <m.hurt@incusperformance.com>
2021-03-14 21:53:11 +0000

    Add more APIException subclasses

--- problemjson/exceptions.py
+++ problemjson/exceptions.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Standard Library
 from http import HTTPStatus as status
 
 # Django
@@ -26,17 +27,60 @@ class APIException(Exception):
         return str(self.title)
 
 
+# HTTP 400 - Bad Request
+class BadRequest(APIException):
+    status_code = status.BAD_REQUEST
+    default_title = _('Malformed request.')
+
+
+# HTTP 401 - Unauthorized
+class AuthenticationFailed(APIException):
+    status_code = status.UNAUTHORIZED
+    default_title = _('Incorrect authentication credentials.')
+
+
+# HTTP 401 - Unauthorized
+class NotAuthenticated(APIException):
+    status_code = status.UNAUTHORIZED
+    default_title = _('Authentication credentials were not provided.')
+
+
+# HTTP 403 - Forbidden
+class Forbidden(APIException):
+    status_code = status.FORBIDDEN
+    default_title = _('You do not have permission to perform this action.')
+
+
+# HTTP 404 - Not Found
 class NotFound(APIException):
     status_code = status.NOT_FOUND
     default_title = _('Not found.')
 
 
+# HTTP 405 - Method Not Allowed
 class MethodNotAllowed(APIException):
     status_code = status.METHOD_NOT_ALLOWED
-    default_detail = _('Method "{method}" not allowed.')
-    default_code = 'method_not_allowed'
+    default_title = _('Method "{method}" not allowed.')
 
     def __init__(self, method, title=None):
         if title is None:
             title = force_str(self.default_title).format(method=method)
         super().__init__(title)
+
+
+# HTTP 406 - Not Acceptable
+class NotAcceptable(APIException):
+    status_code = status.NOT_ACCEPTABLE
+    default_title = _('Could not satisfy the request Accept header.')
+
+
+# HTTP 415 - Unsupported Media Type
+class UnsupportedMediaType(APIException):
+    status_code = status.UNSUPPORTED_MEDIA_TYPE
+    default_title = _('Unsupported media type "{media_type}" in request.')
+
+
+# HTTP 429 - Too Many Requests
+class Throttled(APIException):
+    status_code = status.TOO_MANY_REQUESTS
+    default_title = _('Request was throttled.')
